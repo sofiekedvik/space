4f071708da0a687a85c62460089d8e696557666a
Sofie kedvik <sofie.kedvik@gmail.com>
2015-09-01 00:32:31 +0200

    fixed the skybox bug

--- js/skybox.js
+++ js/skybox.js
@@ -1,10 +1,10 @@
 function createSky(){
 	var skyGeo = new THREE.SphereGeometry(50000, 25, 25);
-	var texture = THREE.ImageUtils.loadTexture( "images/space.jpg" );
+	var texture = THREE.ImageUtils.loadTexture( "images/stars4.jpg" );
 	texture.wrapS = THREE.RepeatWrapping;
 	texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 1, 1 );
-	var material = new THREE.MeshPhongMaterial({
+	var material = new THREE.MeshBasicMaterial({
 		ambient: "black",
 		map: texture,
 		depthWrite: false}
